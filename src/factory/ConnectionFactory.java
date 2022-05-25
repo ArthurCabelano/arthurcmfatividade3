c2e7dce4936a7fca7d307455bdf68005fee8708d
Arthur <Arthur@DESKTOP-4M60D3M>
2022-05-25 20:46:04 -0300

    atividade3

--- src/factory/ConnectionFactory.java
+++ src/factory/ConnectionFactory.java
@@ -2,29 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package factory;
-
 /**
  *
  * @author Arthur
  */
 
-// situa em qual package ou "pacote" está a classe 
-//package br.com.projeto.factory; 
-// faz as importações de classes necessárias para o funcionamento do programa 
+package factory;
 import java.sql.Connection; 
 // conexão SQL para Java 
 import java.sql.DriverManager; 
 // driver de conexão SQL para Java 
-import java.sql.SQLException; 
-// classe para tratamento de exceções 
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-     public Connection getConnection() {
+     public  Connection getConnection() {
 		 try {
-			return DriverManager.getConnection("jdbc:mysql://localhost/projetojava","seu-nome-de-usuario","sua-senha");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/arthurcmfatividade3?serverTimezone=UTC","root","SENHA@123");
 		 }         
-		 catch(SQLException excecao) {
-			throw new RuntimeException(excecao);
+		 catch(SQLException xxx) {
+			throw new RuntimeException(xxx);
 		 }
      }
+     
+    
+    
 }
